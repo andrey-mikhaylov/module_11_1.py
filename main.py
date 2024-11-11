0f7c0f3f95ddeac842da44bc03e01420f429426b
Andrey <andrey.mikhaylov.n@gmail.com>
2024-11-11 12:09:15 +0300

    requests, pandas

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
-import requirements
+try:
+    import requests
+    import pandas
+    import numpy
+    import matplotlib
+    import PIL
+except:
+    print('please install libraries using:')
+    print('pip install -r requirements.txt')
+    exit(1)
 
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pprint
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def test_requests():
+    print('testing requests')
+    query1 = 'urban university'
+    query2 = query1.replace(' ', '%20')
+    headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
+    response = requests.get('https://google.com/search', headers=headers, params={'q': query1})
+    response.raise_for_status()
+    t = response.text.split(f'data-async-context="query:{query2}"')[1].split('bottomads')[0].split('href="')
+    for i, r in enumerate(t):
+        print(i, r.split('"')[0])
 
 
-# Press the green button in the gutter to run the script.
+def test_pandas():
+    print('testing pandas')
+    mydataset = {
+      'cars': ["BMW", "Volvo", "Ford"],
+      'passings': [3, 7, 2]
+    }
+    myvar = pandas.DataFrame(mydataset)
+    print(myvar)
+
+
+def test_numpy():
+    print('testing numpy')
+
+
+def test_matplotlib():
+    print('testing matplotlib')
+
+
+def test_pillow():
+    print('testing pillow')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    test_requests()
+    test_pandas()
+    test_numpy()
+    test_matplotlib()
+    test_pillow()
